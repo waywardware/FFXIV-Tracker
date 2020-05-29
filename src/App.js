f713884e0bdb81e0913c31e3be99a0a199916673
Angel Bouzo <angelb7574@gmail.com>
2020-05-29 15:03:29 -0700

    adding basic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Search } from './features/search/Search'
-import { Mounts } from './features/mounts/Mounts'
-import { Grid } from '@material-ui/core'
 import './App.css';
+import { Route, HashRouter } from 'react-router-dom';
+import MountFarmPage from './pages/MountFarmPage';
 
 function App() {
   return (
-    <Grid className="grid" direction="row" container spacing={2} alignItems="flex-start" justify="flex-start">
-      <Grid item xs={12} sm={5} md={3} lg={2} xl={2}>
-        <Search />
-      </Grid>
-      <Grid item xs={12} sm={7} md={9} lg={10} xl={10}>
-        <Mounts />
-      </Grid>
-    </Grid>
+    <HashRouter>
+      <div>
+        <div className="content">
+          <Route path="/" component={MountFarmPage} />
+        </div>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/pages/MountFarmPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Search } from '../features/search/Search'
+import { Mounts } from '../features/mounts/Mounts'
+import { Grid } from '@material-ui/core'
+import './MountFarmPage.css';
+
+function MountFarmPage() {
+  return (
+    <Grid className="grid" direction="row" container spacing={2} alignItems="flex-start" justify="flex-start">
+      <Grid item xs={12} sm={5} md={3} lg={2} xl={2}>
+        <Search />
+      </Grid>
+      <Grid item xs={12} sm={7} md={9} lg={10} xl={10}>
+        <Mounts />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default MountFarmPage;
