d5bb578d10fe25f4e6d1380d3b9799de9cef2918
Angel Bouzo <angelb7574@gmail.com>
2020-08-05 20:40:30 -0700

    adding player unpin (#40)

--- src/features/mounts/Mounts.js
+++ src/features/mounts/Mounts.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { selectMounts, selectAppliedFilters, POSSIBLE_FILTERS, selectShowMounts, selectAreMountsLoading } from './mountsSlice'
+import { selectMounts, selectAppliedFilters, POSSIBLE_FILTERS, selectShowMounts, selectAreMountsLoading, playerRemove } from './mountsSlice'
 import { toggleObtained, toggleFilter } from "./mountsSlice"
-import { GridListTile, GridList, Avatar, Paper, Typography, Grid, Chip, makeStyles, Tooltip } from '@material-ui/core'
+import { GridListTile, GridList, Avatar, Paper, Typography, Grid, Chip, makeStyles, Tooltip, IconButton } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
 import { ProgressBar } from '../../components/progressBar/ProgressBar'
 
 const useStyles = makeStyles({
     mountResult: {
+        position: 'relative',
         overflow: 'auto',
         padding: '8pt',
         marginBottom: '6pt',
@@ -34,6 +36,11 @@ const useStyles = makeStyles({
     mountList: {
         marginTop: '4pt',
         marginBottom: '4pt'
+    },
+    unPin: {
+        position: "absolute",
+        top: "0pt",
+        left: "0pt",
     }
 })
 
@@ -63,6 +70,9 @@ export function Mounts() {
         <ProgressBar isLoading={isLoading} />
         {isMountsReadyForDisplay ? mounts.map(player =>
             <Paper className={classes.mountResult} elevation={3} key={player.playerId}>
+                <IconButton className={classes.unPin} aria-label="remove" onClick={() => dispatch(playerRemove({ playerId: player.playerId }))}>
+                    <CloseIcon />
+                </IconButton>
                 <Grid container direction="row" justify="flex-start" alignItems="center" alignContent="center">
                     <Grid container item lg={1} md={2} direction="column" alignItems="center">
                         <Grid item xs={12}>
